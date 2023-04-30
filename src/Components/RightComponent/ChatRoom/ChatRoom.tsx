365fc524dc1a89b36471e88b5cc9c9cdee73022b
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-04-30 22:27:31 +0800

    [GF-11] Lint 너무 깐깐함;

--- src/Components/RightComponent/ChatRoom/ChatRoom.tsx
+++ src/Components/RightComponent/ChatRoom/ChatRoom.tsx
@@ -3,12 +3,9 @@ import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
   MainContainer,
   ChatContainer,
-  ConversationHeader,
-  Avatar,
   MessageList,
   Message,
   MessageInput,
-  TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 
 export function ChatRoom() {
